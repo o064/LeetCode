c0ce842076e6f1c1f543bbf64196f3308fcb5f18
Omar Mohamed <85836816+o064@users.noreply.github.com>
2025-07-28 01:06:27 +0300

    Time: 637 ms (22.29%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -5,12 +5,12 @@ WHERE tiv_2015 IN(
     SELECT tiv_2015 
     FROM Insurance 
     GROUP BY tiv_2015 
-    HAVING COUNT(tiv_2015) > 1
+    HAVING COUNT(*) > 1
 ) AND 
 (lat,lon) NOT IN (
     SELECT lat , lon 
     FROM Insurance 
     GROUP BY lat , lon 
-    HAVING COUNT(lat) > 1
+    HAVING COUNT(*) > 1
 )
 ;
