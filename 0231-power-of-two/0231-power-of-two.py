906203705119e642f779a20824cf8e1b46bb5334
Omar Mohamed <85836816+o064@users.noreply.github.com>
2025-08-03 23:52:54 +0300

    Time: 0 ms (100%), Space: 12.4 MB (80.31%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if(n == 1):
+            return True;
+        elif(n < 1 or  n%2 == 1):
+            return False;
+        else :
+            return  self.isPowerOfTwo(n//2);
+
+        
