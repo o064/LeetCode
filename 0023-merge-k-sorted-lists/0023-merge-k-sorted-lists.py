bfeade93618f52086a9bcce261ca29de46b64a8e
Omar Mohamed <85836816+o064@users.noreply.github.com>
2025-08-16 21:36:35 +0300

    Time: 15 ms (36.57%), Space: 20.1 MB (89.86%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,39 +1,35 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeKLists(self, lists):
-        """
-        :type lists: List[Optional[ListNode]]
-        :rtype: Optional[ListNode]
-        """
-        def merge(left , right):
-            dummyHead = merged
-            while left and right : 
-                if left.val <= right.val:
-                    dummyHead.next = left 
-                    dummyHead = dummyHead.next
-                    left = left.next
-                else :
-                    dummyHead.next = right 
-                    dummyHead = dummyHead.next
-                    right = right.next
-            while left : 
-                    dummyHead.next = left 
-                    dummyHead = dummyHead.next
-                    left = left.next
-            while right :
-                    dummyHead.next = right 
-                    dummyHead = dummyHead.next
-                    right = right.next
-        while len(lists)  > 1:
-            right = lists.pop()
-            left = lists.pop()
-            merged = ListNode()
-            merge(left,right)
-            merged = merged.next
-            lists.append(merged)
-        return lists[0] if lists else None
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists or len(lists) == 0:
+            return None
 
+        while len(lists) > 1:
+            mergedLists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                mergedLists.append(self.mergeList(l1, l2))
+            lists = mergedLists
+        return lists[0]
+    def mergeList(self, l1, l2):
+        dummy = ListNode()
+        tail = dummy
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        if l1:
+            tail.next = l1
+        if l2:
+            tail.next = l2
+        return dummy.next
+        
