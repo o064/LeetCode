2003fbae673da9f6d3b600e23c07a6e93bb9200c
Omar Mohamed <85836816+o064@users.noreply.github.com>
2025-08-24 12:27:30 +0300

    Time: 89 ms (74.92%), Space: 27 MB (83.89%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,17 +1,11 @@
-class Solution(object):
-    def dailyTemperatures(self, temperatures):
-        """
-        :type temperatures: List[int]
-        :rtype: List[int]
-        """
-        n =len(temperatures)
-        answer = [0]*n
-        st = [0]
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack =[0]
+        n=  len(temperatures)
+        ans = [0] * n
         for i in range(1,n):
-            while st and temperatures[st[-1]] < temperatures[i] :
-                answer[st[-1] ] =i-st[-1] 
-                st.pop()
-            st.append(i)
-        return answer
-
-
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                idx = stack.pop()
+                ans[idx] = i - idx
+            stack.append(i)
+        return ans 
