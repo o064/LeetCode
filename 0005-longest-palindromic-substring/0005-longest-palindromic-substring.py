88bdd940e4f1354994c6cd5296e3818aea56d26b
Omar Mohamed <85836816+o064@users.noreply.github.com>
2025-08-29 15:46:50 +0300

    Time: 2682 ms (22.33%), Space: 25.3 MB (15.18%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,23 +1,26 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        maxLen = 0
-        start = 0
-        n =len(s)
+        n= len(s)
+        dp = [[False] * n for _ in range(n)]  # 2D array initialized with false
         for i in range(n):
-            l , r = i,i
-            while l >= 0 and r < n and s[l] == s[r] :
-                if (r-l+1) > maxLen: 
-                    maxLen = r-l+1
-                    start=l
-                l -=1
-                r +=1
-            l , r = i,i+1
-            while l >= 0 and r < n and s[l] == s[r] :
-                if (r-l+1) > maxLen: 
-                    maxLen = r-l+1
-                    start=l
-                l -=1
-                r +=1
+            dp[i][i] = True   # set diagonal to True  -> single ch  is palidrome
+        maxLen = 1
+        start = 0
+        for L in range(2,n+1) :
+            for i in range(n-L+1) : 
+                j = i + L - 1
+                if L == 2:
+                    dp[i][j] = (s[i]==s[j])
+                else : 
+                    dp[i][j] = (s[i]==s[j] and dp[i+1][j-1])
+                if dp[i][j] and L > maxLen:
+                    maxLen = L
+                    start = i 
         return s[start : start + maxLen]
+
+
+
+
+
 
             
