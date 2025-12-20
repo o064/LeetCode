2d80d952ef842c2c5cbb52e084f1e5ba7106e1ac
Omar Mohamed <85836816+o064@users.noreply.github.com>
2025-12-20 18:04:10 +0200

    Time: 3 ms (47.88%), Space: 18.2 MB (99.92%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -4,9 +4,9 @@ class Solution:
         # start from 1 
         # while j == i remove nums[j]
         # return nums.length
-        i = 1
-        for j in range(1 , len(nums)):
+        i = 1 
+        for j in range(1, len(nums)):
             if nums[j] != nums[i-1]:
-                nums[i] = nums[j]
-                i += 1
-        return i
+                nums[i] = nums[j] 
+                i+=1
+        return i 
