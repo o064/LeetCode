658b1c485ea3c3ab3cbfdf9d02e2e02a02385802
Omar Mohamed <85836816+omar-shahieen@users.noreply.github.com>
2025-12-22 20:13:11 +0200

    Time: 63 ms (81.91%), Space: 172.8 MB (91.5%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,66 +1,71 @@
-struct Node {
-    int key, value;
+struct Node{
+    int key;
+    int value;
     Node* prev;
     Node* next;
-    Node(int k, int v) : key(k), value(v), prev(nullptr), next(nullptr) {}
+    Node(int key ,int value){
+        this->key=key;
+        this->value =value;
+        prev = next = nullptr;
+    }
 };
 
 class LRUCache {
     unordered_map<int, Node*> mp;
-    Node* head;
+    Node* head; 
     Node* tail;
-    int capacity;
+    int capacity; 
+    // remove to the list 
     void remove(Node* node){
-        if(node->prev) node->prev->next= node->next;
-        else head = node->next;
-        if(node->next) node->next->prev = node->prev;
-        else tail =node->prev;
+        if(node->prev) node->prev->next = node->next;
+        else head  = node->next;
+        if(node->next) node->next->prev =node->prev;
+        else tail = node->prev;
+
     }
     void append(Node* node){
         node->prev = tail;
         node->next = nullptr;
 
-        if(tail) tail->next=node;
-        tail =node;
-        
+        if(tail) tail->next =node;
+        tail = node;
         if(!head) head = node;
     }
 public:
     LRUCache(int capacity) {
-        head = tail =nullptr;
         this->capacity =capacity;
+        head =tail = nullptr;
     }
+
     
     int get(int key) {
-        if(!mp.count(key)) return -1;
-        Node *node =mp[key];
-
+    // get the value of key if exist else -1  
+        if(!mp.count(key))
+            return -1; 
+        Node* node = mp[key];
         remove(node);
         append(node);
-
         return node->value;
-        
     }
     
     void put(int key, int value) {
-
+        // update value of if exist else add to cache
+        // if capacity exceeded  remove the least recently used
         if(mp.count(key)){
-            Node* node = mp[key];
+            Node *node =mp[key];
             node->value =value;
             remove(node);
-            append(node); 
+            append(node);
         }else{
             if(mp.size() == capacity){
                 mp.erase(head->key);
                 remove(head);
             }
-            Node *node = new Node(key,value);
-            append(node);
-            mp[key] = node;
+            Node* newNode = new Node(key,value);
+            append(newNode);
+            mp[key]= newNode;
 
         }
-        
-        
     }
 };
 
