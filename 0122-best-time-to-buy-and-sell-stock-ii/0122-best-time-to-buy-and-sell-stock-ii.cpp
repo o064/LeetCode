7561b0af1053746ad5684dfd651b9e42cf939832
Omar Mohamed <85836816+omar-shahieen@users.noreply.github.com>
2025-12-26 19:58:05 +0200

    Time: 0 ms (100%), Space: 17.1 MB (62.38%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -3,12 +3,19 @@ public:
     int maxProfit(vector<int>& prices) {
         int profit = 0;
 
-        int l= 0 , r =1;
+        int low = prices[0];
+        int high = prices[0];
+        int i= 0;
+        int n =prices.size();
+        while(i < n - 1){
+            while(i < n -1  && prices[i] >= prices[i+1])
+                i+=1;
+            low = prices[i];
+            while(i < n -1  && prices[i] <= prices[i+1])
+                i+=1;
+            high = prices[i];
 
-        while(r < prices.size()){
-            if(prices[r] > prices[l])
-                profit += (prices[r] -prices[l]);
-            l++,r++;
+            profit += (high-low);
 
         }
         return profit;
