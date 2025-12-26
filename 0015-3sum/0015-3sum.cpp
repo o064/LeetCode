c345626f5515d1297c85e0f988438e003b4aff8c
Omar Mohamed <85836816+omar-shahieen@users.noreply.github.com>
2025-12-26 16:43:50 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        sort(nums.begin(),nums.end());
+        int n = nums.size() ;
+        for(int i = 0 ; i < n-2 ; i++){
+            int target = - nums[i];
+            int j = i+1;
+            int k = n-1;
+            while(j < k){
+                if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicate i
+
+                int sum = nums[j] + nums[k];
+                if(sum == target){
+                    res.push_back({nums[i],nums[j],nums[k]});
+                    
+                    j++;
+                    k--;
+                }
+                else if(sum < target)
+                    j++;
+                else
+                    k--;
+                
+            }
+        }
+        return res;
+
+
+    }
+};
