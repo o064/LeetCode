5b2f9355fc3f56af170ba8698e62c9e023433d54
Omar Mohamed <85836816+omar-shahieen@users.noreply.github.com>
2025-12-27 11:34:38 +0200

    Time: 3 ms (91.47%), Space: 14.9 MB (85.78%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,32 +17,38 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        Node* copyHead = new Node(0);
         auto curr = head; 
-        auto currCopy =copyHead;
-        unordered_map<Node* , Node*> mp;
         while(curr){
-            auto newNode= new Node(curr->val);
-            newNode->next = curr->next;
-            mp[curr] =  newNode ; 
-            currCopy->next = newNode ;
-            currCopy = currCopy->next;
-            curr = curr->next;
-        }
-        currCopy = copyHead->next;
-        curr = head; 
-        while(currCopy){
-            auto it = mp.find(curr->random);
-            if(it != mp.end()){
-                currCopy->random = it->second;
-            }else{
-                currCopy->random = nullptr;
 
-            }
-            currCopy = currCopy->next;
-            curr = curr->next;
+            auto copy = new Node(curr->val);
+
+            copy->next = curr->next;
+            curr->next = copy;
+
+            curr =curr->next->next;
         }
 
-        return copyHead->next;
+        curr =head;
+
+        while(curr){
+            if(curr->random)
+                curr->next->random = curr->random->next;
+            curr =curr->next->next;
+        }
+
+        auto dummy = new Node(0);
+        auto copy = dummy;
+        curr =head;
+
+        while(curr){
+            copy->next = curr->next;
+            curr->next = curr->next->next;
+
+            curr =curr->next;
+            copy = copy->next;
+
+        }
+
+        return dummy->next;
     }
 };
